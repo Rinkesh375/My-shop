b44647399d8c5d7dd7a52464ef3e1be629693693
Rinkesh375 <rinkeshujjwal16@gmail.com>
2023-06-04 22:27:21 +0530

    Api_Deployment

--- src/theme/color.jsx
+++
@@ -1,7 +0,0 @@
-const colors = {
-    brand:{
-        1100:" #f9d3e8",
-        1200:"#ff147b"
-    }
-}
-export default colors
--- src/theme/theme.jsx
+++
@@ -1,3 +0,0 @@
-import colors from "./color";
-import { extendTheme } from "@chakra-ui/react";
-export const customTheme = extendTheme({colors})
